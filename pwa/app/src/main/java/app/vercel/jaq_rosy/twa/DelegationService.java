859b68ba918cf99a14f6c81b4a86fbf67626cf51
Estevão A Santos <ealves1710@hotmail.com>
2025-11-09 04:41:13 -0300

    feat/rec pass v1

---
+++ pwa/app/src/main/java/app/vercel/jaq_rosy/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.jaq_rosy.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
